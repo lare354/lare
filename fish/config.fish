ee1763d50b81c262a891203f4448752c8ebfa978
Lare <linuskb@hotmail.com>
2022-01-13 05:32:35 +0100

    update config

--- fish/config.fish
+++ fish/config.fish
@@ -8,7 +8,6 @@ function fish_prompt
 	alias bright="bash /home/lare/bin/bright"
 	alias weather="curl 'wttr.in/Kramfors?format=4'"
 	alias umbra='ssh umbra@172.27.123.23 -p 22'
-	alias midnight='ssh midnight@172.27.162.2 -p 22'
 	alias top='bpytop'
 	alias du='ncdu'
 	alias df='duf'
