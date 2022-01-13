fbbcf5e4768879535c16ed6b15c02a6b96ab3981
Lare <linuskb@hotmail.com>
2022-01-13 05:29:39 +0100

    update fish and ncspot configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,15 @@
 function fish_prompt
-    set_color e305d9
-    echo -n (whoami)(set_color normal)@(hostname | cut -d . -f 1 )\ (set_color $fish_color_cwd)(prompt_pwd)(set_color normal) '> '
+	set_color e305d9
+	echo -n (whoami)(set_color normal)@(hostname | cut -d . -f 1 )\ (set_color $fish_color_cwd)(prompt_pwd)(set_color normal) '> '
 
-    alias ls='exa'
-    alias dc='cd'
-    alias sl='exa'
-    alias bright="bash /home/lare/bin/bright"
-    alias weather="curl 'wttr.in/Kramfors?format=4'"
-    alias umbra='ssh umbra@172.27.123.23 -p 22'
-    alias midnight='ssh midnight@172.27.162.2 -p 22'
-    alias top='bpytop'
+	alias ls='lsd'
+	alias dc='cd'
+	alias sl='lsd'
+	alias bright="bash /home/lare/bin/bright"
+	alias weather="curl 'wttr.in/Kramfors?format=4'"
+	alias umbra='ssh umbra@172.27.123.23 -p 22'
+	alias midnight='ssh midnight@172.27.162.2 -p 22'
+	alias top='bpytop'
+	alias du='ncdu'
+	alias df='duf'
 end
